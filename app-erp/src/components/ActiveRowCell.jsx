7f0cc76808fb985418272b96cf8d2641a09ca27d
Kayesh <kayesh@example.com>
2026-04-05 23:28:23 +0600

    Auto commit

--- app-erp/src/components/ActiveRowCell.jsx
+++
@@ -1,7 +0,0 @@
-const ActiveRowCell = ({
-  text,
-  status,
-  inactiveClass = "text-red-500 line-through",
-}) => <span className={status === false ? inactiveClass : undefined}>{text}</span>;
-
-export default ActiveRowCell;
--- app-erp/src/pages/purchase/preturn/ReturnPage.jsx
+++
@@ -1,9 +0,0 @@
-const ReturnPage = () => {
-    return (
-        <div>
-            <h1>Return Page</h1>
-        </div>
-    );
-};
-
-export default ReturnPage;
