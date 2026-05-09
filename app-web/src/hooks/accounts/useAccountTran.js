e6fffefec88bfcf75e0c975f5dad34db39cfb05d
Kayesh <kayesh@example.com>
2026-05-09 08:32:54 +0600

    Auto commit

---
+++ app-web/src/hooks/accounts/useAccountTran.js
@@ -0,0 +1,14 @@
+const useAccountTran = () => {
+  const mvuch_stats_Options = [
+    {
+      label: "Posted",
+      value: "Posted",
+    },
+    {
+      label: "Approved",
+      value: "Approved",
+    },
+  ];
+  return null;
+};
+export default useAccountTran;
---
+++ app-web/src/hooks/accounts/useParties.js
@@ -0,0 +1,18 @@
+const useParties = () => {
+  const party_ptype_Options = [
+    {
+      label: "Vendor",
+      value: "Vendor",
+    },
+    {
+      label: "Employee",
+      value: "Employee",
+    },
+    {
+      label: "Bank",
+      value: "Bank",
+    },
+  ];
+  return null;
+};
+export default useParties;
